05c366223e406f522dec512c784bd518cf827a64
Yasir Khan <yasirkhanone22@gmail.com>
2023-11-05 21:36:36 +0500

    Scrimba practice started.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World again react.js</h1>
-      <h2>Simple React app for practice.</h2>
+      <Navbar/>
     </div>
   );
 }
